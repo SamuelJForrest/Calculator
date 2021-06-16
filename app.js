1bd71e550b9b570821dae1483ffec40826eb31c9
Samuel J Forrest <samueljamesforrestweb@gmail.com>
2021-06-16 21:05:54 +0100

    added key input functionality and cleaned up JS file

--- app.js
+++ app.js
@@ -34,18 +34,26 @@ for (let i = 0; i < numberBtns.length; i++){
 }
 
 //Event listeners
-delBtn.addEventListener('click', () => {
+delBtn.addEventListener('click', deleteFunction);
+
+function deleteFunction(){
     numOne = [];
     numTwo = [];
     calcArray = [];
     numDisplay.textContent = '';
+    firstNumber = numOne;
+    secondNumber = numTwo;
+    storedNumber.textContent = firstNumber;
     numDisplay.classList.remove('add');
     numDisplay.classList.remove('subtract');
     numDisplay.classList.remove('multiply');
     numDisplay.classList.remove('divide');
-});
+    numDisplay.classList.remove('number-one-stored');
+}
 
-divide.addEventListener('click', () => {
+divide.addEventListener('click', divideFunction);
+
+function divideFunction(){
     numDisplay.classList.add('number-one-stored');
     numDisplay.classList.add('divide');
     calcArray.push(firstNumber);
@@ -55,9 +63,11 @@ divide.addEventListener('click', () => {
     numDisplay.classList.remove('add');
     numDisplay.classList.remove('subtract');
     numDisplay.classList.remove('multiply');
-});
+};
 
-multiply.addEventListener('click', () => {
+multiply.addEventListener('click', multiplyFunction);
+
+function multiplyFunction(){
     numDisplay.classList.add('number-one-stored');
     numDisplay.classList.add('multiply');
     calcArray.push(firstNumber);
@@ -67,9 +77,11 @@ multiply.addEventListener('click', () => {
     numDisplay.classList.remove('add');
     numDisplay.classList.remove('subtract');
     numDisplay.classList.remove('divide');
-});
+};
 
-add.addEventListener('click', () => {
+add.addEventListener('click', addFunction);
+
+function addFunction(){
     numDisplay.classList.add('number-one-stored');
     numDisplay.classList.add('add');
     calcArray.push(firstNumber);
@@ -79,9 +91,11 @@ add.addEventListener('click', () => {
     numDisplay.classList.remove('subtract');
     numDisplay.classList.remove('multiply');
     numDisplay.classList.remove('divide');
-});
+};
 
-subtract.addEventListener('click', () => {
+subtract.addEventListener('click', subtractFunction);
+
+function subtractFunction(){
     numDisplay.classList.add('number-one-stored');
     numDisplay.classList.add('subtract');
     calcArray.push(firstNumber);
@@ -91,12 +105,18 @@ subtract.addEventListener('click', () => {
     numDisplay.classList.remove('add');
     numDisplay.classList.remove('multiply');
     numDisplay.classList.remove('divide');
-})
+}
 
-equals.addEventListener('click', () => {
+equals.addEventListener('click', equalsFunction);
+
+function equalsFunction(){
     if (numDisplay.classList.contains('divide')){
         let dividedNumber = parseInt(firstNumber) / parseInt(secondNumber);
-        numDisplay.textContent = dividedNumber;
+        if (dividedNumber % 1 === 0){
+            numDisplay.textContent = dividedNumber;
+        } else {
+            numDisplay.textContent = dividedNumber.toFixed(2);
+        }
         firstNumber = dividedNumber;
     } else if (numDisplay.classList.contains('multiply')){
         let multipliedNumber = parseInt(firstNumber) * parseInt(secondNumber);
@@ -115,4 +135,142 @@ equals.addEventListener('click', () => {
     numDisplay.classList.remove('number-one-stored');
     numTwo = [];
     storedNumber.textContent = '';
-})
+}
+
+
+document.onkeypress = function(e){
+    console.log(e.which);
+    if (e.which === 48){
+        if (!numDisplay.classList.contains('number-one-stored')){
+            numOne.push(0);
+            numOne.slice(' ');
+            firstNumber = numOne.join('');
+            numDisplay.textContent = firstNumber; 
+        } else {
+            numTwo.push(0);
+            numTwo.slice(' ');
+            secondNumber = numTwo.join('');
+            numDisplay.textContent = secondNumber; 
+        }  
+    } else if (e.which === 49){
+        if (!numDisplay.classList.contains('number-one-stored')){
+            numOne.push(1);
+            numOne.slice(' ');
+            firstNumber = numOne.join('');
+            numDisplay.textContent = firstNumber; 
+        } else {
+            numTwo.push(1);
+            numTwo.slice(' ');
+            secondNumber = numTwo.join('');
+            numDisplay.textContent = secondNumber; 
+        }  
+    } else if (e.which === 50){
+        if (!numDisplay.classList.contains('number-one-stored')){
+            numOne.push(2);
+            numOne.slice(' ');
+            firstNumber = numOne.join('');
+            numDisplay.textContent = firstNumber; 
+        } else {
+            numTwo.push(2);
+            numTwo.slice(' ');
+            secondNumber = numTwo.join('');
+            numDisplay.textContent = secondNumber; 
+        }  
+    } else if (e.which === 51){
+        if (!numDisplay.classList.contains('number-one-stored')){
+            numOne.push(3);
+            numOne.slice(' ');
+            firstNumber = numOne.join('');
+            numDisplay.textContent = firstNumber; 
+        } else {
+            numTwo.push(3);
+            numTwo.slice(' ');
+            secondNumber = numTwo.join('');
+            numDisplay.textContent = secondNumber; 
+        }  
+    } else if (e.which === 52){
+        if (!numDisplay.classList.contains('number-one-stored')){
+            numOne.push(4);
+            numOne.slice(' ');
+            firstNumber = numOne.join('');
+            numDisplay.textContent = firstNumber; 
+        } else {
+            numTwo.push(4);
+            numTwo.slice(' ');
+            secondNumber = numTwo.join('');
+            numDisplay.textContent = secondNumber; 
+        }  
+    } else if (e.which === 53){
+        if (!numDisplay.classList.contains('number-one-stored')){
+            numOne.push(5);
+            numOne.slice(' ');
+            firstNumber = numOne.join('');
+            numDisplay.textContent = firstNumber; 
+        } else {
+            numTwo.push(5);
+            numTwo.slice(' ');
+            secondNumber = numTwo.join('');
+            numDisplay.textContent = secondNumber; 
+        }  
+    } else if (e.which === 54){
+        if (!numDisplay.classList.contains('number-one-stored')){
+            numOne.push(6);
+            numOne.slice(' ');
+            firstNumber = numOne.join('');
+            numDisplay.textContent = firstNumber; 
+        } else {
+            numTwo.push(6);
+            numTwo.slice(' ');
+            secondNumber = numTwo.join('');
+            numDisplay.textContent = secondNumber; 
+        }  
+    } else if (e.which === 55){
+        if (!numDisplay.classList.contains('number-one-stored')){
+            numOne.push(7);
+            numOne.slice(' ');
+            firstNumber = numOne.join('');
+            numDisplay.textContent = firstNumber; 
+        } else {
+            numTwo.push(7);
+            numTwo.slice(' ');
+            secondNumber = numTwo.join('');
+            numDisplay.textContent = secondNumber; 
+        }  
+    } else if (e.which === 56){
+        if (!numDisplay.classList.contains('number-one-stored')){
+            numOne.push(8);
+            numOne.slice(' ');
+            firstNumber = numOne.join('');
+            numDisplay.textContent = firstNumber; 
+        } else {
+            numTwo.push(8);
+            numTwo.slice(' ');
+            secondNumber = numTwo.join('');
+            numDisplay.textContent = secondNumber; 
+        }  
+    } else if (e.which === 57){
+        if (!numDisplay.classList.contains('number-one-stored')){
+            numOne.push(9);
+            numOne.slice(' ');
+            firstNumber = numOne.join('');
+            numDisplay.textContent = firstNumber; 
+        } else {
+            numTwo.push(9);
+            numTwo.slice(' ');
+            secondNumber = numTwo.join('');
+            numDisplay.textContent = secondNumber; 
+        }  
+    } else if (e.which === 13){
+        equalsFunction();
+    } else if (e.which === 99){
+        deleteFunction();
+    } else if (e.which === 47){
+        divideFunction();
+    } else if (e.which === 42){
+        multiplyFunction();
+    } else if (e.which === 43){
+        addFunction();
+    } else if (e.which === 45){
+        subtractFunction();
+    }
+}
